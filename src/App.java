113a0b75995f79b8501ec4e31c4acc3cc8c271d2
Devon Hulse <devhulse04@icloud.com>
2022-09-14 17:32:48 -0400

    mysecond chnage

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! fortnite battee royle");
         System.out.println("this is the change to the code");
+        System.out.println("This is my second change lol git stuff");
     }
 }
